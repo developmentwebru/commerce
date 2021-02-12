88b1d545a79fabbb7f3c1c7db505ab535e3ca3e0
sergeisaz <sergeisaz53@gmail.com>
2021-02-12 17:36:13 +0300

    first commit

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from "@material-ui/core";
+
+const products =[
+    { id: 1, name: 'Shoes', description: 'Running shoes.' },
+    { id: 2, name: 'MacBook', description: 'Apple macBook.' }
+]
+
+const Product = () => {
+   return (
+       <div></div>
+   )
+}
+
+export default Product;
